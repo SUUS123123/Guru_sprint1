4b001931d9e937395f7c0a100824abf0b6dab4b8
admin <tigrankipshiev@gmail.com>
2026-03-25 10:49:09 +0300

    GS 1_2

---
+++ src/lesson!_tassk@.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var applePrice: Double = 25.5
+    var applesAmount: Int = 15
+
+    var totalPrice = applePrice * applesAmount
+
+    totalPrice += totalPrice * 0.05
+
+    println("Итоговая стоимость: $totalPrice")
+}
